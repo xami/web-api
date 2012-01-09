741dc89f67fe01bbad65a791a9b5f79507e78db4
xami520@qq.com <xami520@qq.com@5eb81020-d506-600b-7bee-59dbd4e27000>
2012-01-09 17:12:37 +0000

    x

---
+++ protected/components/WpRemote.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: UU
+ * Date: 12-1-8
+ * Time: 下午11:42
+ * To change this template use File | Settings | File Templates.
+ */
+
+class WpRemote
+{
+    private $client;
+    private $wpURL;
+    private $ixrPath;
+
+    private $uname;
+    private $upass;
+
+    public $postID;
+    public $cateID;
+
+    function __construct($domain, $uname, $upass)
+    {
+        $this->uname=$uname;
+        $this->upass=$upass;
+
+        $this->wpURL='http://'.$domain.'/xmlrpc.php';
+        $this->ixrPath=Yii::getPathOfAlias(
+            'application.extensions.wordpress.wp-includes'
+        ).DIRECTORY_SEPARATOR.'class-IXR.php';
+        include_once($this->ixrPath);
+        $this->client = new IXR_Client($this->wpURL);
+    }
+
+    public function postContent($content)
+    {
+        if(!is_array($content)) throw new CException('Invalid Argument');
+
+        if(!$this->client->query('metaWeblog.newPost','',$this->uname,$this->upass,$content,true))
+            throw new CException($this->client->getErrorMessage());
+
+        $this->postID = $this->client->getResponse();
+        return $this->postID;
+    }
+
+    public function getCate($categoryName='')
+    {
+        if(!is_string($categoryName) || empty($categoryName)) throw new CException('Invalid Argument');
+
+        if(!$this->client->query('metaWeblog.getCategories',0 ,$this->uname,$this->upass))
+            throw new CException($this->client->getErrorMessage());
+        $cateArray = $this->client->getResponse();
+        foreach($cateArray as $catOne){
+            if($catOne['categoryName']==$categoryName) {
+                $this->cateID = $catOne['categoryId'];
+                return $this->cateID;
+            }
+        }
+
+        $newCate=array(
+            'name' => $categoryName,
+            'slug' => $categoryName,
+            'parent_id' => 0,
+            'description' => $categoryName
+        );
+        if(!$this->client->query('wp.newCategory', 0,$this->uname, $this->upass, $newCate))
+            throw new CException($this->client->getErrorMessage());
+        $this->cateID = $this->client->getResponse();
+        return $this->cateID;
+    }
+}
---
+++ protected/extensions/doc360.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Lj
+ * Date: 12-1-4
+ * Time: 下午2:12
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Doc360 extends CApplicationComponent{
+    protected $_src;
+
+    protected function  getDocSrc($sid){
+        $sid=intval($sid);
+
+        if(empty($sid)) return false;
+        $src='http://www.360doc.com/showWeb/0/0/'.$sid.'.aspx';
+        $o = Tools::OZCurl($src, 600, false);
+
+        $this->_src='';
+        if(isset($o['Header']['7']) && substr($o['Header']['7'], 0, 9)=='Location:'){
+            $this->_src=trim(substr($o['Header']['7'], 9));
+        }
+        if(empty($this->_src)) return false;
+        return $this->_src;
+    }
+
+    public function getDocInfo(){
+        if(empty($this->_src)) return false;
+    }
+
+    public function getSid(){
+        $i=Crontab::model()->findBySql('select `sid` from `crontab` ORDER BY `sid` DESC limit 1');
+        if(empty($i)){
+            return 1000;
+        }else{
+            return $i->sid+1;
+        }
+    }
+
+    public function Post2Wp($sid, $domain, $user, $pass){
+        $sid=intval($sid);
+        if($sid<1) return false;
+        $src=$this->getDocSrc($sid);
+
+        $crontab=Crontab::model()->find('sid=:sid',array(':sid'=>$sid));
+        if(empty($crontab)){
+            $crontab=new Crontab();
+            $crontab->status=0;
+            $crontab->msg='';
+            $crontab->error='';
+            $crontab->sid=$sid;
+            $crontab->pid=0;
+        }
+
+        if(empty($src)){
+            $crontab->status=-1;
+            $crontab->msg='此页面不存在';
+            $crontab->error='404';
+            $crontab->pid=0;
+            $crontab->save();
+            throw new CException('此页面不存在');
+        }
+
+        $o = Tools::OZCurl($src, 600, false);
+        if(empty($o['Result'])){
+            $crontab->status=-1;
+            $crontab->msg='页面为空';
+            $crontab->error='500';
+            $crontab->pid=0;
+            $crontab->save();
+            throw new CException('页面为空');
+        }
+
+        include_once(
+            Yii::getPathOfAlias(
+                'application.extensions.simple_html_dom'
+            ).DIRECTORY_SEPARATOR.'simple_html_dom.php'
+        );
+        $html = str_get_html($o['Result']);
+        $description=Tools::subString_UTF8(trim($html->find('span[id=articlecontent]', 0)->plaintext),0,200);
+        $body=trim($html->find('span[id=articlecontent]', 0)->innertext);
+        $title=trim($html->find('title', 0)->plaintext);
+
+        $keywords=preg_split('/[+-,\/\\\s?;.\[\]\<\>]+/', $title, -1, PREG_SPLIT_NO_EMPTY);
+        $keywords[]=trim($html->find('td[class=mz]', 0)->plaintext);
+        $keywords=implode(',', $keywords);
+
+        $category=trim($html->find('span[class=bulebold bulelink]', 0)->plaintext, '[]');
+        $wp = new WpRemote($domain, $user, $pass);
+        //取得分类成功（没有则自动创建）
+        if($wp->getCate($category)){
+            $publish = array(
+                'title'=>$title,
+                'description'=>$body,
+                'mt_allow_comments'=>1,  // 1 to allow comments
+                'mt_allow_pings'=>1,		// 1 to allow trackbacks
+                'mt_excerpt'=>$description,
+                'post_type'=>'post',
+                'mt_keywords'=>$keywords,
+                'categories'=>array($category),
+                'custom_fields' => array(
+                    array("key"=>"description","value"=>"$description"),
+                    array("key"=>"keyword","value"=>"$keywords"),
+                    array("key"=>"sid","value"=>"$sid"),
+                    array("key"=>"author","value"=>"yardown.com"),
+                )
+            );
+            if($pid=$wp->postContent($publish)){
+                $crontab->status=1;
+                $crontab->msg="成功发布: $title | ID :$pid";
+                $crontab->error=serialize(array('pid'=>$pid,'sid'=>$sid));
+                $crontab->pid=$pid;
+                $crontab->save();
+                return true;
+            }else{
+                $crontab->status=-1;
+                $crontab->msg="发布失败: $title | SID :$sid";
+                $crontab->error=serialize(array('pid'=>$pid));
+                $crontab->pid=0;
+                $crontab->save();
+                throw new CException('发布帖子失败');
+            }
+        }else{
+            $crontab->status='505';
+            $crontab->msg='创建分类失败';
+            $crontab->error='';
+            $crontab->save();
+            throw new CException('创建分类失败');
+        }
+    }
+}
